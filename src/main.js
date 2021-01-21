848ae299fd5ce3ca773c5f2ba6e19ecf07f7cb34
softtiger <revokeyu@hotmail.com>
2021-01-21 09:32:15 +0800

    before test image loader

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-import $ from 'jquery'
-
 import './css/index.css'
 
-import './css/index.less'
-
 import './css/index.scss'
-
-$(function(){
-    $('li:odd').css('backgroundColor','lightblue')
-    $('li:even').css('backgroundColor','yellow')
-}) 
