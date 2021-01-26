018685d65541b8ca19d1ce6fc6b63b18c990b382
softtiger <revokeyu@hotmail.com>
2021-01-26 17:16:03 +0800

    use render for vue

---
+++ src/login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>登陆组件</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue  from  'vue'
 
+import login from  './login.vue'
+
 var vm = new Vue({
     el: '#app',
     data: {
         'greet': "hello world"
-    }
+    },
+    render: c => c(login)
 })
